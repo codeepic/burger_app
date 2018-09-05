2f4d0b70fd4dfad939e1ed0a6e33bfaedf4d1367
codeepic <ksaweryglab@gmail.com>
2018-09-05 23:23:13 +0100

    feat: make toggle

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
             {name: "Max", age: 38},
             {name: "Manu", age: 20},
             {name: "Stephanie", age: 26}
-        ]
+        ],
+        showPeople: false
     };
 
     switchNameHandler = (i, name) => {
@@ -26,28 +27,48 @@ class App extends Component {
         });
     };
 
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: "Maxddd", age: 38},
+                {name: event.target.value, age: 20},
+                {name: "Stephaniddd e", age: 26}
+            ]
+        })
+    };
+
+    togglePeopleDiv = () => {
+        //toggle .people divs
+        const shows = this.state.showPeople;
+        this.setState({showPeople: !shows})
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Burger App</h1>
 
                 <p>
-                    <button onClick={this.switchNameHandler.bind(this, 0, 'Mariola')}>Switch Name</button>
+                    <button onClick={this.togglePeopleDiv}>Toggle</button>
                 </p>
 
-                <div className="people">
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}
-                        switchName={(name) => this.switchNameHandler(0, name)} />
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        switchName={this.switchNameHandler.bind(this, 1, 'Lucas')}>My hobbies: racing</Person>
-                    <Person
-                        name={this.state.persons[2].name}
-                        switchName={this.switchNameHandler.bind(this, 2, 'Agnes')} />
-                </div>
+                {
+                    this.state.showPeople ?
+                        <div className="people">
+                            <Person
+                                name={this.state.persons[0].name}
+                                age={this.state.persons[0].age}
+                                switchName={(name) => this.switchNameHandler(0, name)} />
+                            <Person
+                                name={this.state.persons[1].name}
+                                age={this.state.persons[1].age}
+                                switchName={this.switchNameHandler.bind(this, 1, 'Lucas')}
+                                changed={this.nameChangedHandler}>My hobbies: racing</Person>
+                            <Person
+                                name={this.state.persons[2].name}
+                                switchName={this.switchNameHandler.bind(this, 2, 'Agnes')} />
+                        </div> : null
+                }
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Person.css';
 //{/*<div className="person" onClick={props.switchName}>*/}
+{/*<div className="person" onClick={() => props.switchName('sjsjsjsjjs')}>*/}
 export const person = (props) => {
     return (
-        <div className="person" onClick={() => props.switchName('sjsjsjsjjs')}>
-            <p>I am a Person. My name is {props.name} and I am {props.age} years old.</p>
+        <div className="person" >
+            <p>
+                I am a Person. My name is <em>{props.name}</em> and I am <em>{props.age}</em> years old.
+            </p>
             <p>{props.children}</p>
+            <p><input type="text" onChange={props.changed} value={props.name} /></p>
         </div>
     )
 };
